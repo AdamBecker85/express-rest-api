1ea08f34ae75f90fabe2b06a27c1f7f67e089c79
Adam Becker <amkbecker@gmail.com>
2022-01-24 18:29:35 +0100

    Init project/ add routing/ add restAPI

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.listen(8000, () => {
+    console.log('Server is running on port: 8000');
+});
+
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+app.use((req, res) => {
+  return res.json({message: 'Not found...'});
+});
