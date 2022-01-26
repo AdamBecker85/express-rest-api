af3e960fc2257d651e9e72476a966da61b08cc97
Adam Becker <amkbecker@gmail.com>
2022-01-26 16:57:05 +0100

    Add dependencies

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 
 const testimonialsRoutes = require('./routes/testimonials.routes');
 const concertsRoutes = require('./routes/concerts.routes');
